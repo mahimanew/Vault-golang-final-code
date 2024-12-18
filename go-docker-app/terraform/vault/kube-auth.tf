631f75533d70c63943cfe48d4f931c58f7c7758b
mahi <mahima2071987@gmail.com>
2024-12-18 12:02:24 +0530

    Update kube-auth.tf

--- go-docker-app/terraform/vault/kube-auth.tf
+++ go-docker-app/terraform/vault/kube-auth.tf
@@ -1,21 +1,10 @@
 provider "vault" {
-  address         = "https://3.83.68.194:8202/" # Vault address
-  token           = ""                          # Vault token, can be stored in a variable or environment variable
+  address         = "https://34.203.226.62:8202/"  # Vault address
+  token           = "" # Vault token, can be stored in a variable or environment variable
   skip_tls_verify = true
 }
 
-provider "aws" {
-  region = "us-east-1" # Update with your AWS region
-}
 
-# Fetch the EKS cluster details
-data "aws_eks_cluster" "cluster" {
-  name = "cluster-test"
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.aws_eks_cluster.cluster.name
-}
 
 resource "vault_policy" "mysecret" {
   name   = "mysecret"
@@ -31,11 +20,7 @@ resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+
 
 # Fetch the ServiceAccount token from the Kubernetes secret
 data "kubernetes_secret" "sa_token" {
@@ -57,7 +42,7 @@ resource "vault_kubernetes_auth_backend_config" "kubernetes_config" {
 resource "vault_kubernetes_auth_backend_role" "vault_role" {
   role_name                        = "vault-role"
   backend                          = vault_auth_backend.kubernetes.path
-  bound_service_account_names      = ["vault-sa"]
+  bound_service_account_names      = ["go-app"] #service account name
   bound_service_account_namespaces = ["vault"]
   token_policies                   = ["default", "mysecret"]
   token_max_ttl                    = 3600 # Max TTL in seconds
